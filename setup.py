9997dc19faa885b9f0149263530462a2d3fd07bd
Mohamed Azab <159525429+mohamedazab224@users.noreply.github.com>
2025-07-10 08:33:26 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='charts',
+    version='1.6.3',
+    description='Charts App',
+    author='Your Name',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
